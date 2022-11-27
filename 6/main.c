5bfe5246d0e76dfb2d53af798fc5573083c3fe10
ParanormalParrot <87578834+ParanormalParrot@users.noreply.github.com>
2022-11-27 23:51:45 +0300

    updates

--- 6/main.c
+++ 6/main.c
@@ -7,48 +7,28 @@ double f(double a, double b, double x) {
 
 double integrate(double a, double b, int lower, int upper) {
     double step = 0.0000001;
-    double res = 0.0;
+    double res = 0;
     for (double i = lower; i < upper; i += step) {
         res += (f(a, b, i + step) + f(a, b, i)) * step / 2;
     }
     return res;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
     double a = 0;
     double b = 0;
     int lower = 0;
     int upper = INT_MIN;
     double res;
-    if (argc != 1 && argc != 2 && argc != 3) {
-        printf("You need to specify 2 files in the command line arguments: input "
-               "and output");
-        return 1;
-    } else if (argc == 1) {
-        printf("Insert a(double):");
-        scanf("%lf", &a);
-        printf("Insert b(double):");
-        scanf("%lf", &b);
-        printf("Inser the lower bound(int):");
-        scanf("%d", &lower);
-        while (upper <= lower) {
-            printf("Insert the upper bound(int):");
-            scanf("%d", &upper);
-        }
-    } else if (argc == 2) {
-        FILE *f = fopen(argv[1], "r");
-        fscanf("%lf", &a);
-        fscanf("%lf", &b);
-        fscanf("%d", &lower);
-        fscanf("%d", &upper);
-        fclose(f);
-    } else {
-        FILE *f = fopen(argv[1], "r");
-        fscanf("%lf", &a);
-        fscanf("%lf", &b);
-        fscanf("%d", &lower);
-        fscanf("%d", &upper);
-        fclose(f);
+    printf("Insert a(double):");
+    scanf("%lf", &a);
+    printf("Insert b(double):");
+    scanf("%lf", &b);
+    printf("Inser the lower bound(int):");
+    scanf("%d", &lower);
+    while (upper <= lower) {
+        printf("Insert the upper bound(int):");
+        scanf("%d", &upper);
     }
     res = integrate(a, b, lower, upper);
     printf("%f", res);
